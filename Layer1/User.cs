ea2fcd3302fc40b95d114beb5fd2aff07c3e3a46
Aleksandar Stoyanov <aleksandar.v.stoyanov@gmail.com>
2019-10-27 23:10:26 +0200

    initial commit

---
+++ Layer1/User.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace PredictorApi.Layer1{
+    public class User {
+        public int Id {get;set;}
+        public string Name {get;set;}
+        public DateTime DateOfBirth {get;set;}
+    }
+}
