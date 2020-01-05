6ca7ffd024b40d4044cdf3b2e987724fb5fbe190
Aleksandar Stoyanov <aleksandar.v.stoyanov@gmail.com>
2020-01-06 00:42:01 +0200

    team update method

--- Controllers/TeamController.cs
+++ Controllers/TeamController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace PredictorApi.Controllers
@@ -16,5 +17,45 @@ namespace PredictorApi.Controllers
         {
             return Ok(_teamService.GetTeams());
         }
+
+        [HttpPost]
+        public IActionResult UpdateTeam([FromBody] TeamDto teamDto)
+        {
+            //return Ok(_teamService.Save(new FormData(form))
+
+
+            return Ok(_teamService.SaveTeam(teamDto));
+        }
+
+        /*
+        [HttpPost]
+        public IActionResult Insert(IFormCollection form)
+        {
+            if (!_channelsService.Security.CanManageChannels)
+                return Challenge();
+            return Ok(_channelsService.Save(new FormData(form)));
+        }
+        */
+        /*[HttpPost]
+         public IActionResult GetAnswer([FromBody] Answer data)
+        {
+            var answer = new Answer()
+            {
+                MailInfoId = data.Id,
+                IsApproved = data.IsApproved,
+                Reason = data.Reason,
+                Comment = data.Comment,
+                IsSpecialPriceList = data.IsSpecialPriceList
+            };
+
+            return Ok(_mailService.GetAnswer(answer));
+        } */
+
+    }
+    public class TeamDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string DisplayName { get; set; }
     }
 }
--- Layer3/TeamService.cs
+++ Layer3/TeamService.cs
@@ -1,10 +1,13 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
+using PredictorApi.Controllers;
 using PredictorApi.Layer1;
 
 public interface ITeamService
 {
     List<Team> GetTeams();
+    Task SaveTeam(TeamDto teamDto);
 }
 
 public class TeamService : ITeamService
@@ -18,6 +21,25 @@ public class TeamService : ITeamService
                 var users = session.Query<Team>().Take(10).ToList();
 
                 return users;
+            }
+        }
+    }
+
+    public Task SaveTeam(TeamDto teamDto)
+    {
+        using (var session = NHibernateHelper.OpenSession())
+        {
+            using (session.BeginTransaction())
+            {
+                var id = teamDto.Id;
+                var team = session.Query<Team>().Where(x => x.Id == id).First();
+
+                team.Name = teamDto.Name;
+                team.DisplayName = teamDto.DisplayName;
+
+                session.SaveOrUpdate(team);
+
+                return Task.CompletedTask;
             }
         }
     }
